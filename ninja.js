396a60b88c519451b9999935e289e6156c9c4e7a
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-10-25 17:36:43 -0700

    initial commit

---
+++ ninja.js
@@ -0,0 +1,27 @@
+class Ninja {
+    constructor(name, health, speed, strength) {
+        this.name = name;
+        this.health = health;
+        this.speed = 3;
+        this.strength = 3;
+    }
+
+    sayName() {
+        console.log(this.name);
+    }
+
+    showStats() {
+        console.log("Name: " + this.name, ", Strength: " + this.strength, ", Speed: " + this.speed, ", Health: " + this.health);
+    }
+
+    drinkSoju() {
+        this.health += 10;
+        console.log("Drinking some Soju...")
+    }
+}
+
+const thisNinja = new Ninja("Gwenpool", 100);
+thisNinja.sayName();
+thisNinja.showStats();
+thisNinja.drinkSoju();
+thisNinja.showStats();
